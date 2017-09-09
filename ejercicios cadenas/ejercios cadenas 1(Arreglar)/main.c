39317c07815f8921b1973fa00379239d86342ecb
Lombard97 <lombard_k-po@hotmail.com>
2017-09-08 21:39:33 -0300

    Ejercicios de cadenas

---
+++ ejercicios cadenas/ejercios cadenas 1(Arreglar)/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int validacadena(char[], int);
+ /*cantidadnombres(char[][20]);*/
+void mostrarnombres char[];
+int main()
+{
+    char mat[10][20];
+
+    char aux [50];
+
+    cantidadnombres(nombre[]cantidad[20])
+
+        while(validacadena(aux, 20)==0)
+        {
+            printf("Nombre no valido, Reingrese");
+            gets(aux);
+        }
+        strcpy(mat[1],aux);
+    for(int i=0;i<5;i++)
+    {
+        printf("%s\n",mat[i]);
+
+    }
+
+}
+
+validacadena(char mat[20], int aux)
+{
+    esvalido = 0;
+
+    if(strlen(mat)<aux)
+    {
+        esvalido=1;
+    }
+
+}
+mostrarnombres(char nombres[][20], int cantidad)
+{
+    for(int i;i=cantidad;i++)
+    {
+        mostrarnombre(nombres[i]);
+    }
+}
