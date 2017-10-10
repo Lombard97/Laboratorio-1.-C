1efb591af893ea82aae636b748650179f55ea16e
Lombard97 <lombard_k-po@hotmail.com>
2017-10-09 21:58:17 -0300

    Memoria dinamica

---
+++ EstructuraDinamica/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char* nombre[];
+    int legajo;
+    int sueldo;
+}eEmpleado;
+
+void new_empleado(char,int,int);
+
+int main()
+{
+    eEmpleado empleado;
+    printf("Hello world!\n");
+    return 0;
+}
+
+new_empleado(char empleado.nombre,empleado.)
+{
+    int cant;
+    char aux[50]
+    printf("Ingrese nombre");
+    gets(aux);
+    printf("Ingrese legajo");
+    scanf("%d",Empleado.legajo);
+}
---
+++ memoriaDinamica/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int* vec;
+    int num;
+    int* aux;
+    vec =(int*)malloc(5*sizeof(int));
+    if(vec==NULL)
+    {
+        printf("No se enmcontro espacio en memoria");
+        exit(1);
+    }
+    for(int i=0;i<5;i++)
+    {
+        printf("Ingrese un numero\n");
+        scanf("%d",&num);
+    }
+    for(int i=0;i<5;i++)
+    {
+        printf("%d",*(vec+i));
+    }
+    printf("\n");
+    free(vec);
+    return 0;
+    aux=(int*)realloc(*vec,10*sizeof(int));
+
+    if(aux!=NULL)
+    {
+        vec=aux;
+    }
+    for(int i=0;i<10;i++);
+    {
+        printf("%d",*(vec+i));
+    }
+    printf("\n");
+    free(vec);*/
+    int cant;
+    char* nombre;
+    char auxcant[50];
+    printf("ingrese nombre: ");
+    cant=(auxcant);
+    cant=strlen(auxcant);
+
+
+}
